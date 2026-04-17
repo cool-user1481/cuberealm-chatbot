14921b00e58d7b66383bb98073f16fc5d05a2ee3
cool-user1481 <9003651@student.munster.us>
2026-04-17 18:40:29 -0500

    gamble.js - leaderboard, heads/tails are optional in coinflip, add singular, format switched to lower case.

--- gamble.js
+++ gamble.js
@@ -13,36 +13,49 @@ setInterval(()=>{
 
 // gamble function is called on every chat message recived with the argument, text, of whatever the server sends.
 function gamble(text) {
+    if(!text.includes(": $gamble")){
+        return;
+    }
+    if(!gambleOn){
+        sendChat("gamble is temporarily unavalible");
+        return;
+    }
+    
     const parts = text.split(": ");
     const user = parts[0];
-    const messageContents = parts.slice(1).join(": ");
-
-    try {
-        if (messageContents.startsWith("$gamble signup") && gambleOn) {
+    const messageContents = parts.slice(1).join(": ").toLowerCase();
+    
+    switch (true){
+        case messageContents.startsWith("$gamble signup"):
             gambleSignup(user, messageContents, gambleData);
-        } else if (messageContents.startsWith("$gamble coinflip ") && gambleOn) {
+            break;
+        case messageContents.startsWith("$gamble coinflip "):
             gambleCoinflip(user, messageContents, gambleData);
-        } else if (messageContents.startsWith("$gamble stats") && gambleOn) {
+            break;
+        case messageContents.startsWith("$gamble stats"):
             gambleStats(user, messageContents, gambleData);
-        } else if (messageContents.startsWith("$gamble daily") && gambleOn) {
+            break;
+        case messageContents.startsWith("$gamble daily"):
             gambleDaily(user, messageContents, gambleData);
-        } else if (messageContents.startsWith("$gamble addMoney") && gambleOn) {
+            break;
+        case messageContents.startsWith("$gamble addmoney"):
             gambleAddMoney(user, messageContents, gambleData);
-        } else if (messageContents.startsWith("$gamble ") && !gambleOn){
-            sendChat("gamble is temporarily unavalible");
-        }
-    } catch {
-        //silent error discarding for like when the message does not have : in it so like no console spam plz.
+            break;
+        case messageContents.startsWith("$gamble top") || messageContents.startsWith("$gamble lb") || messageContents.startsWith("$gamble leaderboard"):
+            gambleLeaderboard(user, messageContents, gambleData);
+            break;
+        default:
+            sendChat("error: your command is invalid. Check spelling.");
     }
 }
 
 function gambleSignup(user, messageContent, gambleData) {
-    if (gambleData[user]) {
+    if (gambleData[user.toLowerCase()]) {
         sendChat(`@${user}, Error, you have already signed up.`);
         return;
     }
 
-    gambleData[user] = {
+    gambleData[user.toLowerCase()] = {
         name: user,
         money: 100,
         upgrades: [0.5, 0, 50], //formatt: % win rate-coinflip, money gained on lost coinflip bet, money gained from daily,
@@ -52,67 +65,82 @@ function gambleSignup(user, messageContent, gambleData) {
 }
 
 function gambleCoinflip(user, messageContent, gambleData) {
-    if (!gambleData[user]) {
+    if (!gambleData[user.toLowerCase()]) {
         sendChat(`@${user}: Error, you appear to not yet have a chabot gamble account. Create one with $gamble signup`);
         return;
     }
 
-    const regex = /^(heads|tails) (\d+)/;
+    const regex = /^(heads |tails |)(\d+)/;
     const match = regex.exec(messageContent.split("$gamble coinflip ")[1]);
+    match[1] = match[1] || "default";
 
-    if (Number(match[2]) > gambleData[user].money) {
-        sendChat(`@${user} You only have $${gambleData[user].money} chatbucks, so you cannot afford that bet.`);
+    if (Number(match[2]) > gambleData[user.toLowerCase()].money) {
+        sendChat(`@${user} You only have $${gambleData[user.toLowerCase()].money} chatbuck${gambleData[user.toLowerCase()].money === "" ? "" : "s"}, so you cannot afford that bet.`);
         return;
     }
 
-    if (Math.random() < gambleData[user].upgrades[0]) {
-        gambleData[user].money += Number(match[2]);
-        sendChat(`@${user}: success! It landed on ${match[1]}. You earned ${match[2]} chatbucks, bringing your total to ${gambleData[user].money}!`);
+    if (Math.random() < gambleData[user.toLowerCase()].upgrades[0]) {
+        gambleData[user.toLowerCase()].money += Number(match[2]);
+        sendChat(`@${user}: success! It landed on ${match[1]}. You earned ${match[2]} chatbuck${match[2] === "1" ? "" : "s"}, bringing your total to ${gambleData[user.toLowerCase()].money}!`);
     } else {
-        gambleData[user].money -= Number(match[2]);
-        gambleData[user].money += Math.floor(Number(match[2]) * (gambleData[user].upgrades[1] / 100));
+        gambleData[user.toLowerCase()].money -= Number(match[2]);
+        gambleData[user.toLowerCase()].money += Math.floor(Number(match[2]) * (gambleData[user.toLowerCase()].upgrades[1] / 100));
 
-        if (gambleData[user].upgrades[1] !== 0) {
-            sendChat(`Oh no! It did not land on ${match[1]} You lost ${match[2]} chatbucks, but got back ${gambleData[user].upgrades[1]}%`);
+        if (gambleData[user.toLowerCase()].upgrades[1] !== 0) {
+            sendChat(`Oh no! It did not land on ${match[1]} You lost ${match[2]} chatbuck${match[2] === "1" ? "" : "s"}, but got back ${gambleData[user.toLowerCase()].upgrades[1]}%`);
         } else {
-            sendChat(`Oh no! It did not land on ${match[1]} You lost ${match[2]} chatbucks.`);
+            sendChat(`Oh no! It did not land on ${match[1]} You lost ${match[2]} chatbuck${match[2] === "1" ? "" : "s"}.`);
         }
     }
 }
 
 function gambleStats(user, messageContent, gambleData) {
-    if (gambleData[user]) {
-        sendChat(`@${user} $${gambleData[user].money} chatbucks, a ${(gambleData[user].upgrades[0]) * 100}% chance of sucess on coinflips, and you get back ${gambleData[user].upgrades[1]}% on a failed bet of over $100.`);
+    if (gambleData[user.toLowerCase()]) {
+        sendChat(`@${user} $${gambleData[user.toLowerCase()].money} chatbuck${gambleData[user.toLowerCase()].money === 1 ? "" : "s"}, a ${(gambleData[user.toLowerCase()].upgrades[0]) * 100}% chance of sucess on coinflips, and you get back ${gambleData[user.toLowerCase()].upgrades[1]}% on a failed bet (rounds down).`);
     } else {
         sendChat(`@${user} Error, you appear to not yet have a chabot gamble account. Create one with $gamble signup`);
     }
 }
 
 function gambleDaily(user, messageContent, gambleData) {
-    if (!gambleData[user]) {
+    if (!gambleData[user.toLowerCase()]) {
         sendChat(`@${user}: Error, you appear to not yet have a chabot gamble account. Create one with $gamble signup`);
         return;
     }
 
-    if (Math.floor(Date.now() / 1000 / 60 / 60 / 24) > gambleData[user].last) {
-        gambleData[user].money += gambleData[user].upgrades[2];
-        gambleData[user].last = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
-        sendChat(`@${user}: Gained $${gambleData[user].upgrades[2]} for daily money, come back tomorrow for more!`);
+    if (Math.floor(Date.now() / 1000 / 60 / 60 / 24) > gambleData[user.toLowerCase()].last) {
+        gambleData[user.toLowerCase()].money += gambleData[user.toLowerCase()].upgrades[2];
+        gambleData[user.toLowerCase()].last = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+        sendChat(`@${user}: Gained $${gambleData[user.toLowerCase()].upgrades[2]} for daily money, come back tomorrow for more!`);
     } else {
         sendChat(`@${user}: You have already collected your daily for today, come back tomorrow`);
     }
 }
 
 function gambleAddMoney(user, messageContent, gambleData){
-    if(["coolussr", "chatbot", "coolussr1481"].includes(user)){
+    if(["coolussr", "chatbot", "coolussr1481", "coolussr1482"].includes(user)){
         let parts = messageContent.trim().split(" ");
         let addTo = parts[2];
         let amount = Number(parts[3]);
         if(isNaN(amount))
         sendChat("Syntax error: NaN");
         else
-        gambleData[addTo].money += amount;
+        gambleData[addTo.toLowerCase()].money += amount;
     } else {
         sendChat("Authentication error: nuh uh");
     }
+}
+
+function gambleLeaderboard(user, messageContent, gambleData) {
+    function getTopNames(o){
+        return Object.values(gambleData)
+            .sort((a, b) => b.money - a.money)
+            .map(item => item.name);
+    }
+    let offset = 0; // Add param later
+    let top5 = getTopNames().slice(offset, 5 + offset);
+    let temp = `top ${offset + 1}-${offset + 5}:       `; // Note: this contains nbsp's for lines to look good
+    for(let i=0; i<5; i++)
+        temp += `${i + 1 + offset}. ${gambleData[top5[i].toLowerCase()].name}": $${gambleData[top5[i + offset].toLowerCase()].money}${i === 4 ? '' : '   '}`; // As does this
+    sendChat(temp);
 }
